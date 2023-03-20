dae5381141b13c2629e2500c4aa055aa2a635396
SSS5900 <sosnovskihsergey@gmail.com>
2023-03-21 01:58:28 +0500

    ParameterizedTest

--- src/test/java/ru/netology/services/MonCoServiceTest.java
+++ src/test/java/ru/netology/services/MonCoServiceTest.java
@@ -2,34 +2,38 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class MonCoServiceTest {
-    @Test
-    public void testI10E3T20() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/MonCo.csv")
+
+    public void testI10E3T20(int income, int expense, int threshold, int expected) {
         MonCoService service = new MonCoService();
 
-        int income = 10_000;
-        int expense = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+//        int income = 10_000;
+//        int expense = 3_000;
+//        int threshold = 20_000;
+//        int expected = 3;
 
         int actual = (int) service.calcMonth(income, expense, threshold);
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-    public void testI100E60T150() {
-        MonCoService service = new MonCoService();
+    //    @Test
+//    public void testI100E60T150() {
+    MonCoService service = new MonCoService();
 
-        int income = 100_000;
-        int expense = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
+//        int income = 100_000;
+//        int expense = 60_000;
+//        int threshold = 150_000;
+//        int expected = 2;
 
-        int actual = (int) service.calcMonth(income, expense, threshold);
-        Assertions.assertEquals(expected, actual);
-    }
+//        int actual = (int) service.calcMonth(income, expense, threshold);
+//        Assertions.assertEquals(expected, actual);
+//    }
 
 
 }
